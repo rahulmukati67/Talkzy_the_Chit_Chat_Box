dfc8a221894ff0dd61ebe2b9054c18f4810c2daa
rahulmukati67 <rahulmukati20067@acropolis.in>
2023-07-17 14:16:08 +0530

    first

---
+++ app/src/main/java/com/example/talkzy_thechit_chatbox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.talkzy_thechit_chatbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
